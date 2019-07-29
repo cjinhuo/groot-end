552be4e5cc54ea7ee96f6e3baa32c0e1a5b35ab2
chenjinhuo <1443970739@qq.com>
2019-07-29 19:33:03 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '', '../static'));
+  await app.listen(3000);
+}
+
+bootstrap();
