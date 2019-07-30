69185f2e7fd1b70bb2980ab75071e562b1650f6e
TypeInfos <1443970739@qq.com>
2019-07-30 21:58:23 +0800

    feat:增加webpack热更新

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '', '../static'));
   await app.listen(3000);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 
 bootstrap();
