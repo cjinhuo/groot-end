eeadecc756ed5202adb69b9fa68d866b4e714167
chenjinhuo <1443970739@qq.com>
2019-08-12 16:53:14 +0800

    feat:更改history为express

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,18 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import history = require('connect-history-api-fallback');
+// tslint:disable-next-line: no-var-requires
+const express = require('express');
 
 // declare const module: any;
 
 async function bootstrap() {
-  // let nativeApp = express();
+  const nativeApp = express();
+  nativeApp.use(history());
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.use(history());
-  // app.useStaticAssets(join(__dirname, '', '../static'));
+  // app.use(history());
   app.useStaticAssets(join(__dirname, '', '../static'));
+  // app.useStaticAssets(join(__dirname, '', '../static'));
   // 启用CORS
   app.enableCors();
   await app.listen(8080);
